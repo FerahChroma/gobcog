c8873e7971bab1dfd151e2ac0c931a0af0ec5bba
Kevin Mok <me@kevin-mok.com>
2019-12-31 11:31:49 -0500

    Fixes #2 and converting epic chests

--- adventure/adventure.py
+++ adventure/adventure.py
@@ -119,6 +119,8 @@ class Adventure(BaseCog):
         self.emojis.skills.bard = (
             "\N{EIGHTH NOTE}\N{BEAMED EIGHTH NOTES}\N{BEAMED SIXTEENTH NOTES}"
         )
+        self.emojis.hp = "\N{HEAVY BLACK HEART}\N{VARIATION SELECTOR-16}"
+        self.emojis.dipl = self.emojis.talk
 
         self._adventure_actions = [
             self.emojis.attack,
@@ -1469,7 +1471,7 @@ class Adventure(BaseCog):
                         box(
                             _(
                                 "Successfully converted {converted} epic treasure "
-                                "chests to {to} legendary treasure chest{plural}. \n{author} "
+                                "chests to {to} legendary treasure chest{plur}. \n{author} "
                                 "now owns {normal} normal, {rare} rare, {epic} epic, "
                                 "{leg} legendary treasure chests and {set} set treasure chests."
                             ).format(
@@ -3407,14 +3409,31 @@ class Adventure(BaseCog):
     async def _choice(self, ctx: Context, adventure_msg):
         session = self._sessions[ctx.guild.id]
 
+        hp = (
+            self.MONSTER_NOW[session.challenge]["hp"]
+            * self.ATTRIBS[session.attribute][0]
+            * self.monster_stats
+        )
+        dipl = (
+            self.MONSTER_NOW[session.challenge]["dipl"]
+            * self.ATTRIBS[session.attribute][1]
+            * self.monster_stats
+        )
         dragon_text = _(
-            "but **a{attr} {chall}** just landed in front of you glaring! \n\n"
+            "but **a{attr} {chall}** ("
+            "{hp_symbol} {hp}/"
+            "{dipl_symbol} {dipl}) "
+            "just landed in front of you glaring! \n\n"
             "What will you do and will other heroes be brave enough to help you?\n"
             "Heroes have 5 minutes to participate via reaction:"
             "\n\nReact with: {reactions}"
         ).format(
             attr=session.attribute,
             chall=session.challenge,
+            hp_symbol=self.emojis.hp,
+            hp=ceil(hp),
+            dipl_symbol=self.emojis.dipl,
+            dipl=ceil(dipl),
             reactions=bold(_("Fight"))
             + " - "
             + bold(_("Spell"))
@@ -3444,7 +3463,9 @@ class Adventure(BaseCog):
             + bold(_("Run")),
         )
         normal_text = _(
-            "but **a{attr} {chall}** "
+            "but **a{attr} {chall}** ("
+            "{hp_symbol} {hp}/"
+            "{dipl_symbol} {dipl}) "
             "is guarding it with{threat}. \n\n"
             "What will you do and will other heroes help your cause?\n"
             "Heroes have 1 minutes to participate via reaction:"
@@ -3452,6 +3473,10 @@ class Adventure(BaseCog):
         ).format(
             attr=session.attribute,
             chall=session.challenge,
+            hp_symbol=self.emojis.hp,
+            hp=ceil(hp),
+            dipl_symbol=self.emojis.dipl,
+            dipl=ceil(dipl),
             threat=random.choice(self.THREATEE),
             reactions=bold(_("Fight"))
             + " - "
